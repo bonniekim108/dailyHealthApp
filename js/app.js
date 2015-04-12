385c8fafce55de58b7bfc474b79608a269d8e647
Bonnie <bonniekim108@yahoo.com>
2015-04-11 18:50:35 -0700

    first commit for app

---
+++ js/app.js
@@ -0,0 +1,4 @@
+var eventInput = document.getElementById("new-event");
+var addButton = document.getElementsByTagName("button")[0];
+var todaysGoalsHolder = document.getElementById("todays-goals");
+var completedGoalsHolder = document.getElementById("completed-goals");
